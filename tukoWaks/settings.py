777b9d1f77d7817db50fcc59e45edb7461639e19
Machel Odhiambo <machelgabriel@gmail.com>
2022-05-15 02:11:52 +0300

    Update the settings file

--- tukoWaks/settings.py
+++ tukoWaks/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'tukoWaks.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tukoworks',
+        'USER': 'chelloh',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
