f0107c3abb8a8b860ac4becafa46da629e61f5cb
Machel Odhiambo <machelgabriel@gmail.com>
2022-05-18 11:38:27 +0300

    Add Views and Url Routes

---
+++ home/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+app_name = "home"
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("works/", views.works, name="works"),
+    path("<int:project_id>/<str:slug>/", views.work_detail, name="work_detail"),
+    path("approach/", views.approach, name="approach"),
+    path("services/", views.services, name="services"),
+    path("contact/", views.contact, name="contact"),
+]
