0dc8986ebc13bbeefaa3464b1b41f1f98f405317
Machel Odhiambo <machelgabriel@gmail.com>
2022-05-25 11:48:48 +0300

    Fix Image Upload Not Working, Edit Settings To Make Adding A Project Successful

--- home/models.py
+++ home/models.py
@@ -9,10 +9,10 @@ class Project(models.Model):
     title = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=100, db_index=True)
     description = models.TextField()
-    images = models.FilePathField(path="/img")
+    images = models.ImageField(upload_to="img")
 
     def __str__(self):
         return self.title
-
+   
     def get_absolute_url(self):
-        return reverse("work_detail", kwargs={"pk": self.pk})
+        return reverse('work_detail', args=[str(self.project_id)])
