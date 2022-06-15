01eb06b51cea68e08f5991f23e422bbb621a2994
Mwai <karimi@tw12ted.fritz.box>
2022-06-15 19:34:29 +0200

    added tests for views

---
+++ home/tests/test_views.py
@@ -0,0 +1,32 @@
+from urllib import response
+from django.test import TestCase
+from django.urls import reverse
+
+from home.models import Project
+
+
+class ProjectListViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        num_projects = 10
+
+        for project_id in range(num_projects):
+            Project.objects.create(project_id=project_id)
+
+    def test_view_url_is_at_correct_place(self):
+        response = self.client.get('/works/')
+        self.assertEqual(response.status_code, 200) 
+
+    def test_view_url_reachable_by_name(self):
+        response = self.client.get(reverse('home:works'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('home:works'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'works.html')
+
+    def test_list_all_authors(self):
+        response = self.client.get(reverse('home:works'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context), 2)
