335323341913592f77ed819d4a907084d93b83c1
Mwai <karimi@tw12ted.fritz.box>
2022-07-20 15:26:40 +0200

    minor change

--- tukoWaks/settings/dev.py
+++ tukoWaks/settings/dev.py
@@ -131,16 +131,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
 
 # simplified static file serving
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
