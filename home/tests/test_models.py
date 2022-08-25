ab905df3bfa041ec1303f9de592bd31ed8175f53
Machel Odhiambo <machelgabriel@gmail.com>
2022-08-25 13:25:28 +0300

    WIP

--- home/tests/test_models.py
+++ home/tests/test_models.py
@@ -1,13 +1,21 @@
-from audioop import reverse
 from django.test import TestCase
+
+
 from home.models import Project
 
-      
+
 class ProjectModelTest(TestCase):
     @classmethod
-    def setUpTestData(cls) -> None:
-       # return super().setUpTestData()
-       Project.objects.create(project_id=1, title='project test', slug='project-test', description='project one description')
+    # def setUpTestData(cls) -> None:
+    # return super().setUpTestData()
+    def setUpTestData(cls):
+        cls.project = Project.objects.create(
+            project_id=1,
+            title='project test',
+            slug='project-test',
+            description='project one description',
+            image='img/project-7.jpg'
+        )
 
     
     def test_project_id(self):
